bf505ae39a60a83f3de6c5b97540f0fe4cd045a9
Braden Stanfield <bls6151@utulsa.edu>
2024-02-22 12:47:14 -0600

    still working on 5 and 6

--- problem#6/fib.py
+++ problem#6/fib.py
@@ -1,6 +1,21 @@
 #Name: Braden Stanfield   Lab: Fri 3
 def fibonacci(n):
     #write your code here
+    num1 = 0
+    num2 = 0
+    num3 = 0
+    i = 0
+    if n == 0:
+        num2 = 0
+    elif n > -1:
+        while i < n:
+            num3 = num2
+            num2 += num1 + num3
+            num1 = num3
+            i += 1
+    else:
+        num2 = -1
+    return num2
 
 
 if __name__ == '__main__':
