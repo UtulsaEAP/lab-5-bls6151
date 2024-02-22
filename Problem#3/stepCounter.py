5b15e91ce637caec3f90dfee97f7e2f0ea81617a
Braden Stanfield <bls6151@utulsa.edu>
2024-02-22 11:55:31 -0600

    Problem 3

--- Problem#3/stepCounter.py
+++ Problem#3/stepCounter.py
@@ -1,9 +1,12 @@
 #Name: Braden Stanfield   Lab: Fri 3
 def feet_to_steps(user_feet):
    #write your code here
+    val = int(user_feet/2.5)
+    return val
 
 if __name__ == '__main__':
     #take input feet steps here
+    user_feet = float(input())
     #store it into the function
-    
+    print(feet_to_steps(user_feet))
     #print your steps here
