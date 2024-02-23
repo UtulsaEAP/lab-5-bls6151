8bad1f0cba40c8284587d6eb729ebc5f5686fe95
Braden Stanfield <bls6151@utulsa.edu>
2024-02-23 15:16:40 -0600

    Problem 6

--- problem#6/fib.py
+++ problem#6/fib.py
@@ -2,20 +2,20 @@
 def fibonacci(n):
     #write your code here
     num1 = 0
-    num2 = 0
+    num2 = 1
     num3 = 0
-    i = 0
+    i = 1
     if n == 0:
-        num2 = 0
+        num3 = 0
     elif n > -1:
         while i < n:
-            num3 = num2
-            num2 += num1 + num3
-            num1 = num3
+            num3 = num1 + num2
+            num1 = num2
+            num2 = num3
             i += 1
     else:
-        num2 = -1
-    return num2
+        num3 = -1
+    return num3
 
 
 if __name__ == '__main__':
